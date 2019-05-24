1915aee000b641ab47fb5c93032d19f200ca5885
hyunuk71 <hyunuk71@users.noreply.github.com>
2019-05-24 09:43:27 -0700

    Refactoring: - (server) struggling with the relationship btw view-viewmodel
    TODO:
    - (client) startProcedure
    - (server) algorithm about sending packets

--- SlimeWarzServer/src/view/ServerApp.java
+++ SlimeWarzServer/src/view/ServerApp.java
@@ -9,19 +9,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.net.InetAddress;
 
 public class ServerApp extends JFrame implements Observer {
 	private final Dimension SERVER_FRAME_DIM = new Dimension(500, 300);
 
 	private Helper helper = Helper.getInstance();
-	private InetAddress ip;
-	private JTextField portText;
 	private JTextArea serverLog;
-	private int portNum;
-	//private ServerModel serverModel;
-	private ServerManager serverManager;
-	private ConnectionManager connectionManager;
+	private ServerManager serverManager = new ServerManager();
+	private ConnectionManager connectionManager = new ConnectionManager();
 
 	public static void main(String[] args) {
 		ServerApp serverApp = new ServerApp();
@@ -40,57 +35,43 @@ public class ServerApp extends JFrame implements Observer {
 		setPreferredSize(SERVER_FRAME_DIM);
 		setResizable(false);
 		setLayout(null);
-		getIP();
 
 		JPanel serverPanel = new JPanel();
-		serverPanel.setLayout(null);
-		helper.attach(this, serverPanel, 0, 200, 500, 300);
 		JLabel currentIPLabel = new JLabel(MSG_CURRENT_IP);
-		JTextField currentIP = new JTextField(getIP());
-		currentIP.setEditable(false);
+		JTextField currentIP = new JTextField(connectionManager.getIP());
 		JLabel portLabel = new JLabel(MSG_PORT_LABEL);
-		portText = new JTextField(MSG_PORT_NUMBER);
+		JTextField portText = new JTextField(MSG_PORT_NUMBER);
+		serverLog = new JTextArea(MSG_SERVER_LOG);
+		JScrollPane sp = new JScrollPane(serverLog);
+		JButton createServerSocketBtn = new JButton(MSG_CREATE_BUTTON);
 
+		serverPanel.setLayout(null);
+		currentIP.setEditable(false);
+		serverLog.setEditable(false);
+		helper.attach(this, serverPanel, 0, 200, 500, 300);
 		helper.attach(this, currentIPLabel, 30, 30, 80, 25);
 		helper.attach(this, currentIP, 110, 30, 100, 25);
 		helper.attach(this, portLabel, 30, 60, 250, 25);
 		helper.attach(this, portText, 280, 60, 50, 25);
-
-		serverLog = new JTextArea();
-		serverLog.setEditable(false);
-		serverLog.append(MSG_SERVER_LOG);
-		JScrollPane sp = new JScrollPane(serverLog);
-		JButton createServerSocketBtn = new JButton(MSG_CREATE_BUTTON);
-
 		helper.attach(this, sp, 30, 150, 400, 100);
 		helper.attach(this, createServerSocketBtn, 30, 100, 150, 40);
 
-		createServerSocketBtn.addActionListener(e -> createServerSocket());
-		addWindowListener(new WindowAdapter() {
+		createServerSocketBtn.addActionListener(e -> {
+			connectionManager.createServer(Integer.parseInt(portText.getText()));
+		});
+
+		this.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				connectionManager.closeServer();
-
 			}
 		});
 		pack();
 		setVisible(true);
 	}
 
-	private void createServerSocket() {
-		portNum = Integer.parseInt(portText.getText());
-		connectionManager.createServer(portNum);
-	}
-
 	private void printLog(String str) {
 		serverLog.append(str + "\r\n");
-	}
-
-	private String getIP() {
-		try {
-			ip = InetAddress.getLocalHost();
-		} catch (Exception ignored) {}
-		return String.valueOf(ip.getHostAddress());
 	}
 
 	@Override
--- SlimeWarzServer/src/viewModel/ConnectionManager.java
+++ SlimeWarzServer/src/viewModel/ConnectionManager.java
@@ -3,9 +3,11 @@ package viewModel;
 import model.ServerModel;
 
 import java.io.IOException;
+import java.net.InetAddress;
 
 public class ConnectionManager {
-	ServerModel serverModel;
+	private ServerModel serverModel;
+	private InetAddress ip;
 
 	public void closeServer() {
 		final String MSG_CLOSING_SERVER = "Closing Server";
@@ -29,5 +31,12 @@ public class ConnectionManager {
 			String errorMSG = "PORT #" + portNum + " is already opened.\r\n";
 			printLog(errorMSG);
 		}
+	}
+
+	public String getIP() {
+		try {
+			ip = InetAddress.getLocalHost();
+		} catch (Exception ignored) {}
+		return String.valueOf(ip.getHostAddress());
 	}
 }
